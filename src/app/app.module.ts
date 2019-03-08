175d303c6e54d08fe5c4646044102bcaed458697
a.didenko <joygromi@gmail.com>
2019-03-08 18:19:22 +0200

    correction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { HotelListComponent } from './hotel-list/hotel-list.component';
 import { HotelWeatherComponent } from './hotel-weather/hotel-weather.component';
 import { HotelProfileComponent } from './hotel-profile/hotel-profile.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HotelListComponent,
     HotelWeatherComponent,
-    HotelProfileComponent
+    HotelProfileComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- src/app/hotel-list/hotel-list.component.ts
+++ src/app/hotel-list/hotel-list.component.ts
@@ -4,18 +4,22 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
-  styleUrls: ['./hotel-list.component.css']
+  styleUrls: ['./hotel-list.component.css'],
 })
 export class HotelListComponent implements OnInit {
   @Input() public hotels: IHotel[];
   @Input() public currentHotel: IHotel;
   @Output() public hotel: EventEmitter<IHotel> = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit() { }
 
   public selectHotel(hotel: IHotel): void {
     this.hotel.emit(hotel);
   }
+
+  
 }
